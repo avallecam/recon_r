f04118fc2eee4056d076a50aba63071dce92fe26
avallecam <avallecam@gmail.com>
2019-06-17 17:21:43 -0500

    ready to run

--- 0-recon.R
+++ 0-recon.R
@@ -4,8 +4,8 @@ p_list <- c("incidence",
             "outbreaker",
             "outbreaker2",
             "projections")
-#install.packages(p_list)
-#devtools::install_github("annecori/EpiEstim")
+install.packages(p_list)
+devtools::install_github("annecori/EpiEstim")
 
 for (i in 1:length(p_list)) {
   library(p_list[i],character.only = T)
